2b4e9b2a58de973cc7535a75d4aa8338f550b32f
najmulhouda <najmulhouda6@gmail.com>
2022-06-21 19:07:01 +0600

    javascript folder added

---
+++ script.js
@@ -0,0 +1,64 @@
+// let js = "amazing";
+// if (js === "amazing") alert("I will be the hero one day");
+
+// console.log(34-3)
+// console.log("najmul")
+
+// let Name = "Najmul houda";
+/*
+console.log(Name);
+console.log(Name);
+console.log(Name);
+console.log(Name);
+console.log(Name);
+console.log(Name);
+
+let iLoveJavascript = true;
+console.log(typeof iLoveJavascript);
+console.log(typeof 34);
+console.log(typeof "Najmul");
+console.log(typeof Name);
+
+Name = "Najmul Houda Mithu"
+console.log(Name);
+
+let age;
+
+
+age = 21;
+console.log(age);
+
+console.log(typeof null);
+console.log(typeof true); 
+
+
+
+const now = 2033;
+const ageNajmul = now - 2001; 
+const ageKirmi = now - 2003;
+console.log(ageNajmul, ageKirmi); 
+
+console.log(ageNajmul * 2, ageKirmi / 2, 2 ** 3);
+
+const firstName = "Najmul";
+const lastName = "Houda";
+
+console.log(firstName +" "+ lastName);
+
+console.log(ageNajmul > ageKirmi);
+console.log(ageKirmi >= 200);
+
+
+
+const massMark = 95;
+const heighMark = 1.88;
+const massJohn = 85;
+const heighJohn = 1.76;
+
+const BMIMark = massMark / heighMark ** 2;
+const BMIJohn = massJohn / heighJohn ** 2;
+const markHigherBMI  = BMIMark > BMIJohn;
+
+console.log(BMIMark, BMIJohn, markHigherBMI);
+*/
+  
